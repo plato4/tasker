4b5d4c2602510a003940065df13d845d6133e7d8
Plato R <platoisalive@gmail.com>
2021-11-01 19:42:55 +0800

    initial commit

---
+++ Tasker/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace Tasker
+{
+    partial class frmTasker
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTaskName = new System.Windows.Forms.TextBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.txtLocation = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.chkOpen = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // txtTaskName
+            // 
+            this.txtTaskName.Location = new System.Drawing.Point(12, 76);
+            this.txtTaskName.Name = "txtTaskName";
+            this.txtTaskName.Size = new System.Drawing.Size(323, 23);
+            this.txtTaskName.TabIndex = 0;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(135, 117);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 1;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // txtLocation
+            // 
+            this.txtLocation.Location = new System.Drawing.Point(12, 28);
+            this.txtLocation.Name = "txtLocation";
+            this.txtLocation.Size = new System.Drawing.Size(323, 23);
+            this.txtLocation.TabIndex = 2;
+            this.txtLocation.Text = "D:/work/tasks";
+            this.txtLocation.TextChanged += new System.EventHandler(this.txtLocation_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(146, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Location";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(146, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Task Name";
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(12, 102);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(0, 15);
+            this.lblInfo.TabIndex = 5;
+            this.lblInfo.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // chkOpen
+            // 
+            this.chkOpen.AutoSize = true;
+            this.chkOpen.Checked = true;
+            this.chkOpen.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkOpen.Location = new System.Drawing.Point(223, 120);
+            this.chkOpen.Name = "chkOpen";
+            this.chkOpen.Size = new System.Drawing.Size(55, 19);
+            this.chkOpen.TabIndex = 6;
+            this.chkOpen.Text = "Open";
+            this.chkOpen.UseVisualStyleBackColor = true;
+            // 
+            // frmTasker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 152);
+            this.Controls.Add(this.chkOpen);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLocation);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.txtTaskName);
+            this.Name = "frmTasker";
+            this.Text = "Tasker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTaskName;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.TextBox txtLocation;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.CheckBox chkOpen;
+    }
+}
+
---
+++ Tasker/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tasker
+{
+    public partial class frmTasker : Form
+    {
+        public frmTasker()
+        {
+            InitializeComponent();
+            try
+            {
+                txtLocation.Text = File.ReadLines("location.txt").First();
+                lblInfo.Text = "location.txt found";
+            }
+            catch (Exception ex)
+            {
+                lblInfo.Text = "location.txt NOT FOUND, RECREATING";
+                txtLocation.Text = "D:/work/tasks";
+                CreateConfig();
+            }
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            if (txtTaskName.Text == String.Empty) txtTaskName.Text = $"Unnamed Task";
+
+            var loc = $"{txtLocation.Text}/{DateTime.Now.Year}{DateTime.Now.Month.ToString().PadLeft(2, '0')}{DateTime.Now.Day.ToString().PadLeft(2, '0')}-{txtTaskName.Text.ToLower().Replace(' ', '-')}";
+
+            if (Directory.Exists(loc)) loc += $"-{DateTime.Now.Ticks}";
+
+            try
+            {
+                Directory.CreateDirectory(loc);
+                lblInfo.Text = loc;
+
+                if (chkOpen.Checked)
+                {
+                    var psi = new ProcessStartInfo() { FileName = loc, UseShellExecute = true };
+                    Process.Start(psi);
+                }
+            }
+            catch (Exception ex)
+            {
+                lblInfo.Text = ex.Message;
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void txtLocation_TextChanged(object sender, EventArgs e)
+        {
+            CreateConfig();
+        }
+
+        private void CreateConfig()
+        {
+            try
+            {
+                File.WriteAllText("location.txt", txtLocation.Text);
+                lblInfo.Text = "location.txt UPDATED";
+            }
+            catch (Exception ex)
+            {
+                lblInfo.Text = $"location.txt {ex.Message}";
+            }
+        }
+    }
+}
